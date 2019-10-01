b4c1c05e30588f2661c225aa12ed7052642d125e
AndroidNetrunner <42410705+AndroidNetrunner@users.noreply.github.com>
2019-10-02 01:29:34 +0900

    Add files via upload

---
+++ rockPaperScissors_blank.js
@@ -0,0 +1,20 @@
+const userChoice = prompt("가위, 바위, 보자기 중 무엇을 선택하시겠습니까?");
+/* input 데이터는 '가위', '바위', '보자기' 중 하나 */
+let computerChoice = Math.random();
+/* computerChoice는 0과 1 사이의 난수 */
+if () {
+/* 채우세요! */
+
+} else if() {
+// 채우세요!
+
+} else {
+// 채우세요!
+}
+console.log();
+// 컴퓨터가 제시한 패를 출력해야 합니다.
+const compare = () => {
+// 컴퓨터와 나의 패를 비교하고 결과를 출력해야 합니다.
+}
+
+console.log(compare(userChoice, computerChoice));
